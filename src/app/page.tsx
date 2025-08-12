1292d43f357bd428d6d283137baf1df98d89cb91
Anna Madeira <69610009+Anna-Madeira@users.noreply.github.com>
2025-08-12 15:29:51 -0300

    feat: Theme and Provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./Routes";
+import { ThemeProvider } from "@mui/material";
+import { LightTheme } from "./Shared/themes";
 export default function App() {
     return (
+      <ThemeProvider theme={LightTheme}>
   <BrowserRouter>
    <AppRoutes />
   </BrowserRouter>
+      </ThemeProvider>
+
 
   
   );
